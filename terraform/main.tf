536647380de561e57f29d45f3ac8af478412550e
Yoganand <yoganand.nandakumar27@gmail.com>
2026-04-15 08:47:44 +0000

    Added IAM role to created EC2 instance for granting access to S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,6 +102,7 @@ resource "aws_instance" "dr_server" {
   ami           = "ami-07216ac99dc46a187" # Ubuntu 22.04 (verify region)
   instance_type = "t3.micro"
   key_name      = "aws_dr_key"
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   subnet_id              = aws_subnet.dr_subnet.id
   vpc_security_group_ids = [aws_security_group.dr_sg.id]
