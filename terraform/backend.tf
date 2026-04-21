204b24812d084891424a198a2d3c9ed3995df5fe
Yoganand <yoganand.nandakumar27@gmail.com>
2026-04-21 21:10:47 +0000

    feat(terraform): dynamic backend + env-based infra deployment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "dr-terraform-state-storage"
-    key            = "dr-infra/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
   }
