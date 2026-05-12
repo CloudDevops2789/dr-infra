07764ae3a052ef9cea8791d08584122e0d91def4
Yoganand <yoganand.nandakumar27@gmail.com>
2026-05-12 20:32:16 +0000

    updated backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "dr-terraform-state-storage-test"
-    key            = "dr-poc/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
   }
